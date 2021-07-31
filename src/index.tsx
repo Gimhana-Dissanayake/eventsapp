22ca88bc42e17a9c2f548cfffe55ce011e5bf37e
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-31 18:57:45 +0530

    59. Scroll to top

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import App from "./app/layout/App";
+import { ScrollToTop } from "./app/layout/ScrollToTop";
 import "./app/layout/styles.css";
 import { configureStore } from "./app/store/configureStore";
 import reportWebVitals from "./reportWebVitals";
@@ -13,6 +14,7 @@ const store = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </Provider>,
