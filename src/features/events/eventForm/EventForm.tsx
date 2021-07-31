6f943c8c354e93317803c993abbdf792cc8a4533
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-31 22:51:31 +0530

    71.Formik props

--- src/features/events/eventForm/EventForm.tsx
+++ src/features/events/eventForm/EventForm.tsx
@@ -59,41 +59,46 @@ export default function EventForm(props: RouteComponentProps) {
           props.history.push("/events");
         }}
       >
-        <Form className="ui form">
-          <Header sub color="teal" content="Event Details" />
-          <MyTextInput name="title" placeholder="Event title" />
-          <MySelectInput
-            name="category"
-            placeholder="Event category"
-            options={categoryData}
-          />
-          <MyTextArea name="description" placeholder="Description" rows={3} />
-          <Header sub color="teal" content="Event Location Details" />
-          <MyTextInput name="city" placeholder="City" />
-          <MyTextInput name="venue" placeholder="Venue" />
-          <MyDateInput
-            name="date"
-            placeholderText="Event date"
-            timeFormat="HH:mm"
-            showTimeSelect
-            timeCaption="time"
-            dateFormat="MMMM d, yyyy h:mm a"
-          />
+        {({ isSubmitting, dirty, isValid }) => (
+          <Form className="ui form">
+            <Header sub color="teal" content="Event Details" />
+            <MyTextInput name="title" placeholder="Event title" />
+            <MySelectInput
+              name="category"
+              placeholder="Event category"
+              options={categoryData}
+            />
+            <MyTextArea name="description" placeholder="Description" rows={3} />
+            <Header sub color="teal" content="Event Location Details" />
+            <MyTextInput name="city" placeholder="City" />
+            <MyTextInput name="venue" placeholder="Venue" />
+            <MyDateInput
+              name="date"
+              placeholderText="Event date"
+              timeFormat="HH:mm"
+              showTimeSelect
+              timeCaption="time"
+              dateFormat="MMMM d, yyyy h:mm a"
+            />
 
-          <Button
-            type="submit"
-            floated="right"
-            positive
-            content="Submit"
-          ></Button>
-          <Button
-            type="submit"
-            floated="right"
-            content="Cancel"
-            as={Link}
-            to="/events"
-          ></Button>
-        </Form>
+            <Button
+              type="submit"
+              floated="right"
+              positive
+              content="Submit"
+              loading={isSubmitting}
+              disabled={!isValid || !dirty || isSubmitting}
+            />
+            <Button
+              disabled={isSubmitting}
+              type="submit"
+              floated="right"
+              content="Cancel"
+              as={Link}
+              to="/events"
+            />
+          </Form>
+        )}
       </Formik>
     </Segment>
   );
