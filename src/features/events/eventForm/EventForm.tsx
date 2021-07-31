a3b260679b1a6d01dd5e1f34f0eacc16e064fb80
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-31 19:24:22 +0530

    62.setting up formik

--- src/features/events/eventForm/EventForm.tsx
+++ src/features/events/eventForm/EventForm.tsx
@@ -1,4 +1,5 @@
 import cuid from "cuid";
+import { Formik } from "formik";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, RouteComponentProps } from "react-router-dom";
@@ -52,75 +53,82 @@ export default function EventForm(props: RouteComponentProps) {
       <Header
         content={selectedEvent ? "Edit the event" : "Create new event"}
       ></Header>
-      <Form onSubmit={handleFormSubmit}>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Event title"
-            name="title"
-            value={values.title}
-            onChange={handleInputChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Category"
-            name="category"
-            value={values.category}
-            onChange={handleInputChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Description"
-            name="description"
-            value={values.description}
-            onChange={handleInputChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="City"
-            name="city"
-            value={values.city}
-            onChange={handleInputChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="text"
-            placeholder="Venue"
-            name="venue"
-            value={values.venue}
-            onChange={handleInputChange}
-          />
-        </Form.Field>
-        <Form.Field>
-          <input
-            type="date"
-            placeholder="Date"
-            name="date"
-            value={values.date}
-            onChange={handleInputChange}
-          />
-        </Form.Field>
-        <Button
-          type="submit"
-          floated="right"
-          positive
-          content="Submit"
-        ></Button>
-        <Button
-          type="submit"
-          floated="right"
-          content="Cancel"
-          as={Link}
-          to="/events"
-        ></Button>
-      </Form>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({ values, handleChange, handleSubmit }) => (
+          <Form onSubmit={handleSubmit}>
+            <Form.Field>
+              <input
+                type="text"
+                placeholder="Event title"
+                name="title"
+                value={values.title}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <input
+                type="text"
+                placeholder="Category"
+                name="category"
+                value={values.category}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <input
+                type="text"
+                placeholder="Description"
+                name="description"
+                value={values.description}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <input
+                type="text"
+                placeholder="City"
+                name="city"
+                value={values.city}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <input
+                type="text"
+                placeholder="Venue"
+                name="venue"
+                value={values.venue}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Form.Field>
+              <input
+                type="date"
+                placeholder="Date"
+                name="date"
+                value={values.date}
+                onChange={handleChange}
+              />
+            </Form.Field>
+            <Button
+              type="submit"
+              floated="right"
+              positive
+              content="Submit"
+            ></Button>
+            <Button
+              type="submit"
+              floated="right"
+              content="Cancel"
+              as={Link}
+              to="/events"
+            ></Button>
+          </Form>
+        )}
+      </Formik>
     </Segment>
   );
 }
