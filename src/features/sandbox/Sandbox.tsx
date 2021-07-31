a20c6281dc2df6ccf17b48a48f9cbdc7f451b6cc
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-07-31 13:28:59 +0530

    redux actions

--- src/features/sandbox/Sandbox.tsx
+++ src/features/sandbox/Sandbox.tsx
@@ -1,14 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "./testReducer";
+import { useDispatch, useSelector } from "react-redux";
+import { Button } from "semantic-ui-react";
+import { DECREMENT_COUNTER, INCREMENT_COUNTER, RootState } from "./testReducer";
 
 const Sandbox = () => {
+  const dispatch = useDispatch();
+
   const data = useSelector((state: RootState) => state.data);
 
   return (
     <>
       <h1>Testing 123</h1>
       <h3>The data is: {`${data}`}</h3>
+      <Button
+        onClick={() => {
+          dispatch({ type: INCREMENT_COUNTER });
+        }}
+        content="Increment"
+        color="green"
+      />
+      <Button
+        onClick={() => {
+          dispatch({ type: DECREMENT_COUNTER });
+        }}
+        content="Decrement"
+        color="red"
+      />
     </>
   );
 };
--- src/features/sandbox/testReducer.ts
+++ src/features/sandbox/testReducer.ts
@@ -1,9 +1,27 @@
+export const INCREMENT_COUNTER = "INCREMENT_COUNTER";
+export const DECREMENT_COUNTER = "DECREMENT_COUNTER";
+
 const initialState = {
   data: 42,
 };
 
-export const testReducer = (state = initialState) => {
-  return state;
+export const testReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return {
+        ...state,
+        data: state.data + 1,
+      };
+
+    case DECREMENT_COUNTER:
+      return {
+        ...state,
+        data: state.data - 1,
+      };
+
+    default:
+      return state;
+  }
 };
 
 export type RootState = ReturnType<typeof testReducer>;
