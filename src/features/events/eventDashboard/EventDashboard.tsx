be723003523230bfdde32664174d2d415df719a9
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-04 00:47:45 +0530

    95.using placeholder to improve the UI

--- src/features/events/eventDashboard/EventDashboard.tsx
+++ src/features/events/eventDashboard/EventDashboard.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
-import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { RootState } from "../../../app/store/rootReducer";
 import EventList from "./EventList";
+import EventListItemPlaceholder from "./EventListItemPlaceholder";
 
 export default function EventDashboard() {
   const { events } = useSelector((state: RootState) => state.event);
   const { loading } = useSelector((state: RootState) => state.async);
 
-  if (loading) return <LoadingComponent />;
-
   return (
     <Grid>
       <Grid.Column width={10}>
+        {loading && (
+          <>
+            <EventListItemPlaceholder />
+            <EventListItemPlaceholder />
+          </>
+        )}
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
