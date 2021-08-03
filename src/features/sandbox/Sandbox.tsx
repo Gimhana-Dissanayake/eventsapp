f53adb045830617af473880b03eab8c1ced3d377
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-03 21:59:51 +0530

    91.isolating the loading indicators

--- src/features/sandbox/Sandbox.tsx
+++ src/features/sandbox/Sandbox.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "semantic-ui-react";
 import { openModal } from "../../app/common/modals/modalReducer";
@@ -7,6 +7,8 @@ import { decrement, increment } from "./testReducer";
 
 const Sandbox = () => {
   const dispatch = useDispatch();
+
+  const [target, setTarget] = useState(null);
 
   const data = useSelector((state: RootState) => state.test.data);
 
@@ -17,17 +19,21 @@ const Sandbox = () => {
       <h1>Testing 123</h1>
       <h3>The data is: {`${data}`}</h3>
       <Button
-        loading={loading}
-        onClick={() => {
+        name="increment"
+        loading={loading && target === "increment"}
+        onClick={(e: any) => {
           dispatch(increment(20));
+          setTarget(e.target.name);
         }}
         content="Increment"
         color="green"
       />
       <Button
-        loading={loading}
-        onClick={() => {
+        name="decrement"
+        loading={loading && target === "decrement"}
+        onClick={(e: any) => {
           dispatch(decrement(10));
+          setTarget(e.target.name);
         }}
         content="Decrement"
         color="red"
