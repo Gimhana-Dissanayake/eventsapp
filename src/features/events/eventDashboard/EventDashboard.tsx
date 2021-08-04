c5cbdeb8454ec9398157494425c0f8959cfd768b
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-04 20:03:26 +0530

    101. Listening to firebase data

--- src/features/events/eventDashboard/EventDashboard.tsx
+++ src/features/events/eventDashboard/EventDashboard.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
+import { getEventsFromFirestore } from "../../../app/firestore/firestoreService";
 import { RootState } from "../../../app/store/rootReducer";
 import EventFilters from "./EventFilters";
 import EventList from "./EventList";
@@ -9,6 +10,17 @@ import EventListItemPlaceholder from "./EventListItemPlaceholder";
 export default function EventDashboard() {
   const { events } = useSelector((state: RootState) => state.event);
   const { loading } = useSelector((state: RootState) => state.async);
+
+  useEffect(() => {
+    const unsubscribe = getEventsFromFirestore({
+      next: (snapshot: any) =>
+        console.log(
+          snapshot.docs.map((docSnapshot: any) => docSnapshot.data())
+        ),
+      error: (error: any) => console.log(error),
+    });
+    return unsubscribe;
+  }, []);
 
   return (
     <Grid>
