d0953b15c6700a1e4caf36f62a88fa5aa1d856c9
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-05 14:22:51 +0530

    115. loggin in

--- src/features/auth/authAction.ts
+++ src/features/auth/authAction.ts
@@ -1,9 +1,17 @@
+import firebase from "../../app/config/firebase";
 import { SIGN_IN_USER, SIGN_OUT_USER } from "./authConstants";
 
-export const signInUser = (payload: any) => {
-  return {
-    type: SIGN_IN_USER,
-    payload,
+export const signInUser = (creds: any) => {
+  return async function (dispatch: any) {
+    try {
+      const result = await firebase
+        .auth()
+        .signInWithEmailAndPassword(creds.email, creds.password);
+
+      dispatch({ type: SIGN_IN_USER, payload: result.user });
+    } catch (error) {
+      throw error;
+    }
   };
 };
 
