f5330669bcb16e1e7f63ed130708f82db0bff1e4
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-09-12 09:01:42 +0530

    159.adding the filter functionality

--- src/features/events/eventDashboard/EventDashboard.tsx
+++ src/features/events/eventDashboard/EventDashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Grid } from "semantic-ui-react";
 import { listenToEventsFromFirestore } from "../../../app/firestore/firestoreService";
@@ -13,6 +13,17 @@ export default function EventDashboard() {
   const dispatch = useDispatch();
   const { events } = useSelector((state: RootState) => state.event);
   const { loading } = useSelector((state: RootState) => state.async);
+
+  const [predicate, setPredicate] = useState(
+    new Map<string, any>([
+      ["startDate", new Date()],
+      ["filter", "all"],
+    ])
+  );
+
+  function handleSetPredicate(key: string, value: any) {
+    setPredicate(new Map(predicate.set(key, value)));
+  }
 
   useFirestoreCollection({
     query: () => listenToEventsFromFirestore(),
@@ -32,7 +43,11 @@ export default function EventDashboard() {
         <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        <EventFilters />
+        <EventFilters
+          predicate={predicate}
+          setPredicate={handleSetPredicate}
+          loading={loading}
+        />
       </Grid.Column>
     </Grid>
   );
--- src/features/events/eventDashboard/EventFilters.tsx
+++ src/features/events/eventDashboard/EventFilters.tsx
@@ -2,17 +2,36 @@ import React from "react";
 import Calendar from "react-calendar";
 import { Header, Menu } from "semantic-ui-react";
 
-const EventFilters = () => {
+const EventFilters = (props: any) => {
   return (
     <>
       <Menu vertical size="large" style={{ width: "100%" }}>
         <Header icon="filter" attached color="teal" content="Filter" />
-        <Menu.Item content="All Events" />
-        <Menu.Item content="I'm going" />
-        <Menu.Item content="I'm hosting" />
+        <Menu.Item
+          content="All Events"
+          active={props.predicate.get("filter") === "all"}
+          onClick={() => props.setPredicate("filter", "all")}
+          disabled={props.loading}
+        />
+        <Menu.Item
+          content="I'm going"
+          active={props.predicate.get("filter") === "isGoing"}
+          onClick={() => props.setPredicate("filter", "isGoing")}
+          disabled={props.loading}
+        />
+        <Menu.Item
+          content="I'm hosting"
+          active={props.predicate.get("filter") === "isHosting"}
+          onClick={() => props.setPredicate("filter", "isHosting")}
+          disabled={props.loading}
+        />
       </Menu>
       <Header icon="calendar" attached color="teal" content="Select date" />
-      <Calendar />
+      <Calendar
+        onChange={(date: Date) => props.setPredicate("startDate", date)}
+        value={props.predicate.get("startDate") || new Date()}
+        tileDisabled={() => props.loading}
+      />
     </>
   );
 };
